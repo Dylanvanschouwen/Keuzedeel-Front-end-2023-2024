cdf220025d51ac2ef39971a88b91a821db9c8399
Dylan <9020515@student.zadkine.nl>
2024-04-23 14:41:58 +0200

    Opdracht 03 done

---
+++ Index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concept Design Applicatie</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <ul>
+                <li><a href="https://dylanvanschouwen.carrd.co/">My Socials</a></li>
+                <li><a href="Https://www.Instagram.com">Instagram</a></li>
+                <li><a href="Https://www.Youtube.com">Youtube</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section class="Section-1">
+            <article id="S-1-A-1">
+                <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
+            </article>
+            <article id="S-1-A-2">
+                <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
+            </article>
+        </section>
+        <section class="Section-2">
+            <article id="S-2-A-1">
+                <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
+            </article>
+        </section>
+        <aside class="Aside-bar">
+            <h2>Side Content</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
+        </aside>
+    </main>
+
+    <footer>
+        <p>&copy; 2024 Your App Name. All rights reserved.</p>
+    </footer>
+</body>
+</html>
