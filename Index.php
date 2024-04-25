767d9a3de15c088464856d33ebbdf7c0cf946e81
Dylan <9020515@student.zadkine.nl>
2024-04-25 15:39:03 +0200

    .

--- Index.php
+++ Index.php
@@ -3,38 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Concept Design Applicatie</title>
+    <title>Portfolio Website</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header>
-        <nav class="navbar">
+        <nav>
             <ul>
-                <li><a href="https://dylanvanschouwen.carrd.co/">My Socials</a></li>
-                <li><a href="Https://www.Instagram.com">Instagram</a></li>
-                <li><a href="Https://www.Youtube.com">Youtube</a></li>
+                <a href="https://dylanvanschouwen.carrd.co/">My Socials</a>
+                <a href="Https://www.Instagram.com">Instagram</a>
+                <a href="Https://www.Youtube.com">Youtube</a>
+                <a href="Https://www.github.com">Github</a>
             </ul>
         </nav>
     </header>
 
     <main>
-        <section class="Section-1">
-            <article id="S-1-A-1">
+        <section class="section-1">
+            <article id="s-1-a-1">
                 <h2>Lorem Ipsum</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
             </article>
-            <article id="S-1-A-2">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
-            </article>
-        </section>
-        <section class="Section-2">
-            <article id="S-2-A-1">
+            <article id="s-1-a-2">
                 <h2>Lorem Ipsum</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
             </article>
         </section>
-        <aside class="Aside-bar">
+        <section class="section-2">
+            <article id="s-2-a-1">
+                <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
+            </article>
+        </section>
+        <aside class="aside-bar">
             <h2>Side Content</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
         </aside>
