1214ad7578454e2a51f65909e4b8207a8e6ae949
Dylan <9020515@student.zadkine.nl>
2024-06-05 13:21:59 +0200

    FRONT-END DONE FUCK IT YEAH

--- Index.php
+++ Index.php
@@ -3,46 +3,160 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio Website</title>
+    <title>Portfolio home page</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header>
         <nav>
+            <h1> Mijn social media links</h1>
             <ul>
                 <a href="https://dylanvanschouwen.carrd.co/">My Socials</a>
-                <a href="Https://www.Instagram.com">Instagram</a>
-                <a href="Https://www.Youtube.com">Youtube</a>
-                <a href="Https://www.github.com">Github</a>
+                <a href="https://www.instagram.com/just_dylan_official/">Instagram</a>
+                <a href="https://www.youtube.com/@just_dylan_official">Youtube</a>
+                <a href="https://github.com/Dylanvanschouwen">Github</a>
             </ul>
         </nav>
     </header>
 
     <main>
-        <section class="section-1">
-            <article id="s-1-a-1">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
-            </article>
-            <article id="s-1-a-2">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
+        <section class="Opening_frame">   
+            <h1 id="Opening_frame_title"> Portfolio van dylan van schouwen </h1>
+            <article id="Opening_frame_text">
+                <p> Welkom op mijn Portfolio Homepagina </p>
+                <p> hier kunt u informatie over mij en mijn werk vinden.</p>
             </article>
         </section>
-        <section class="section-2">
-            <article id="s-2-a-1">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
-            </article>
-        </section>
-        <aside class="aside-bar">
-            <h2>Side Content</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget sapien sed urna accumsan cursus.</p>
-        </aside>
+        <div id="Frame_divider">
+            <section class="Info_frame">
+                <h2 id="Info_frame_title"> Een klein beetje over mij </h2>
+                <article id="Info_frame_text">
+                    <h3 id="Para_info_title">Wie is Dylan van schouwen nou?</h3>
+                    <p> Ik ben een 18 jaar oude student die een opleiding doet op het TCR. 
+                    <br>Ik ben geboren in het IJsselland ziekenhuis op 23 maart 2006.
+                    <br>Ik woon nu in Achthuizen maar ik leefde een groot gedeelte in Ommoord.</p>
+                </article>
+                <article id="Info_frame_text">
+                    <h3 id="Para_info_title"> Hoe ben ik gekomen waar ik nu ben </h3>
+                    <p> Eerst wist ik niet wat ik wou doen met mijn toekomst, het duurde best lang tot ik 
+                        <br> wist dat ik een Software Developer wou worden. Maar toen ik mijn laatste jaar
+                        <br> in het RGO Beroepscampus deed, had ik mijn passie voor het programmeren ontdenkt.
+                        <br> Uiteindelijk had ik besloten om naar het Techniek College Rotterdam te gaan 
+                        <br>dus nu studeer ik om een Software developer te worden.
+                    </p>
+                </article>
+                <h2 id="Info_frame_title"> Mijn oude werk</h2>
+                <article id="Info_frame_text">
+                <h3 id="Para_info_title"> Mijn beste werk tot nu toe </h3>
+                <img src="./Images/First_Project.png" alt="First project image" id="Info_frame_image">
+                <p> Voor dit schoolproject moesten we in een groep van 3 personen werken.
+                    <br>Iedere persoon moest met een gesamelijke database een eigen CRUD project maken.
+                    <br>Na een tijdje proberen had ik het eindelijk gelukt om een CRUD pagina te maken die er goed uit zag.
+                </p>
+                </article>
+                <article id="Info_frame_text">
+                    <h3 id="Para_info_title"> Mijn eerste website </h3>
+                    <img src="./Images/Second_Project.png" alt="Second project image" id="Info_frame_image">
+                    <p> Mijn eerste website was een informatie pagina over de ouderavond van school. 
+                        <br> daarom dachten ik en mijn groep dat het slim was om de TCR website na te bouwen.
+                        <br> met wat tegenslagen was het uiteindelijk wel gelukt
+                    </p>
+                </article>
+                <article id="Info_frame_text">
+                    <h3 id="Para_info_title"> De webpage game die ik heb gemaakt </h3>
+                    <img src="./Images/Third_Project.png" alt="Third project image" id="Info_frame_image_game">
+                    <p> De game die ik heb gemaakt is een game die ik gemaakt heb met HTML, CSS en JavaScript.
+                        <br> dit was best moeilijk om te doen want dit was de eerste keer dat ik JavaScript gebruitk heb
+                        <br> maar uiteindelijk lukte het wel om een Tic-Tac-Toe spel te maken.
+                     </p>
+                </article>
+            </section>
+            <section class="Extra_frame">
+                <div class="Extra_frame_divider">
+                    <div id="Extra_frame_title">
+                        <h1> Dylan van Schouwen </h1>
+                    </div>
+                    <div id="Extra_frame_picture">
+                        <img src="./Images/Profile_picture.png" alt="Profile picture">
+                    </div>
+                </div>
+                <div class="Extra_frame_divider">
+                    <div id="Extra_frame_title">
+                        <h1> Personal Skills </h1>
+                    </div>
+                    <div id="Extra_frame_Personal_skills">
+                        <ul id="Extra_frame_personal_skills_1">
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                            <li>4</li>
+                            <li>5</li>
+                        </ul>
+                        <ul id="Extra_frame_personal_skills_2">
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                            <li>4</li>
+                            <li>5</li>
+                        </ul>
+                    </div> 
+                </div>
+                <div class="Extra_frame_divider">
+                    <div id="Extra_frame_title_family_hobbies">
+                        <h1> family and hobbies </h1>
+                    </div>
+                    <div class="Extra_frame_family_hobbies">
+                        <div id="Extra_frame_Nationaliteit">
+                            <h2>Nationaliteit:</h2>                        
+                            <p>Nederlands</p>
+                        </div> 
+                        <div id="Extra_frame_familie">
+                            <h2>Familie:</h2>
+                            <ul>
+                                <p>Mark van Schouwen (vader)</p> <br>
+                                <p>Sohraya van Schouwen (moeder)</p> <br>
+                                <p>Arwyn van Schouwen (zus)</p> <br>
+                                <p>Tommy (hond)</p>
+                            </ul>
+                        </div>
+                        <div id="Extra_frame_baan">
+                            <h2>Baan:</h2>
+                            <ul>
+                                <p>Medewerker bij de Aldi</p> <br>
+                                <p>Medewerker bij de Action (gestopt)</p>
+                            </ul>
+                        </div>
+                        <div id="Extra_frame_opleiding">
+                            <h2>Opleiding:</h2>
+                            <ul>                            
+                                <p>Software development opleiding op het TCR </p> <br>
+                                <p>PIE opleiding op het IJsselcollege </p> <br>
+                                <p>PIE opleiding op het RGO Beroepscampus</p>
+                            </ul>
+                        </div>  
+                        <div id="Extra_frame_hobbies">
+                            <h2>Hobbies:</h2>                        
+                            <ul>
+                                <p>Anime kijken</p> <br>
+                                <p>Webdevelopment</p> <br>
+                                <p>Games spelen </p>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     </main>
-
-    <footer>
-        <p>&copy; 2024 Your App Name. All rights reserved.</p>
-    </footer>
+    <footer class="footer-container">
+        <div id="footer-left">
+            <h3>Dylan van Schouwen</h3>
+            <p>Software Developer</p>
+        </div>
+        <div id="footer-right">
+            <p>&copy; 2024 Dylan van Schouwen. All rights reserved.</p>
+            <p>Made with &hearts; in Rotterdam</p>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
